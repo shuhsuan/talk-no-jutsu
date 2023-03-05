c5cd7685c42d0ee58b770347d572261254566a29
shuhsuan <shuhsuanchang.9@gmail.com>
2023-03-06 00:53:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas, faPaperPlane);
+
+
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const theInput = document.getElementById("input");
+  const messages = document.getElementById("messages");
+
+  const handleChange = (e) => {
+    if (e.key === "Enter") {
+      theInput.value = "";
+      messages.innerHTML += "<p>" + input + "</p>";
+    }
+  };
+
+  const handleClick = () => {
+    messages.innerHTML = "<p>" + input + "</p>";
+    theInput.value = "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Your username"></input>
+      <div className="inputBit">
+        <input
+          id="input"
+          type="text"
+          onInput={(e) => setInput(e.target.value)}
+          onKeyDown={handleChange}
+          autoComplete="off"
+        ></input>
+        <button id="sendButton" onClick={handleClick}>
+          <FontAwesomeIcon icon="fa-solid fa-paper-plane" />
+        </button>
+      </div>
+
+      <div id="messages"></div>
     </div>
   );
 }
